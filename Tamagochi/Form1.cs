25c34a91ff33639c5018d8d8b52712afe26a9cf5
digital_banana@outlook.com <digital_banana@outlook.com>
2021-04-04 11:59:36 +0300

    Добавьте файлы проекта.

---
+++ Tamagochi/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tamagochi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            init_game();
+        }
+
+        private void init_game()
+        {
+
+        }
+
+
+        private void set_scales()
+        {
+
+        }
+
+
+
+        private void game_over_actions()
+        {
+
+        }
+
+        private void btnEat_Click(object sender, EventArgs e)
+        {
+   
+        }
+
+
+        private void btnSleep_Click(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void btnHappy_Click(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
